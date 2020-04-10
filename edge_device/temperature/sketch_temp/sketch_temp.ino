285d1841913e29f728456ce5bdca9c988808004d
Harman <H.singh92@outlook.com>
2020-04-10 17:21:52 +1000

    Changed sketch to work with OneWire and Dallas libraries

--- edge_device/temperature/sketch_temp/sketch_temp.ino
+++ edge_device/temperature/sketch_temp/sketch_temp.ino
@@ -1,83 +1,29 @@
-// SPI and SD libraries. SPI for connecting SD card to SPI bus.
-#include <SPI.h>
-#include <SD.h>
-const int sdPin = 4;
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-// Temperature pin set to analog 0
-const int temPin = 0;
+#define ONE_WIRE_BUS 5
 
-// Delay time. How often to take a temperature reading, in miliseconds
-// 20 minutes = 1200000 milliseconds
-const int delayTime = 1200000;
+OneWire oneWire(ONE_WIRE_BUS);
 
-// File variable
-File tempsFile;
+DallasTemperature sensors(&oneWire);
 
-
-
-void setup() {
-  // Serial output for when connected to computer
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  Serial.print("Initializing SD card...");
-  if(!SD.begin(sdPin)) {
-    Serial.println("initialization failed!");
-    return;
-  }
-  Serial.println("Initialization done.");
-
-  tempsFile = SD.open("temps.txt", FILE_WRITE);
-
-  if (tempsFile) {
-    Serial.println("Printing temperatures");
-    tempsFile.println("Printing temperatures:");
-    tempsFile.close();
-    Serial.println("Done.");
-  } else {
-    Serial.println("Error opening file in setup.");
-  }
-
-}
-
-void loop() {
-  /********************/
-  // Open SD card for writing
-  tempsFile = SD.open("temps.txt", FILE_WRITE);
-
-  if (tempsFile) {
-    // Temperature readings
-    float voltage, degreesC, degreesF;
-    voltage = getVoltage(temPin);
-    degreesC = (voltage - 0.5) * 100.0;
-    degreesF = degreesC * (9.0/5.0) + 32.0;
-
-    // write temps to Serial
-    Serial.print("Celsius: ");
-    Serial.print(degreesC);
-    Serial.print(" Fahrenheit: ");
-    Serial.println(degreesF);
-
-    // write temps to SD card
-    tempsFile.print("Celsius: ");
-    tempsFile.print(degreesC);
-    tempsFile.print(" Fahrenheit: ");
-    tempsFile.println(degreesF);
-
-    // close the file
-    tempsFile.close();
-  } else {
-    Serial.println("Error opening file in loop.");
-  }
-
-
-  delay(delayTime);
-
-}
-
-float getVoltage(int pin)
+ float Celcius=0;
+ float Fahrenheit=0;
+void setup(void)
 {
-  return (analogRead(pin) * 0.004882814);
+  
+  Serial.begin(9600);
+  sensors.begin();
+}
+
+void loop(void)
+{ 
+  sensors.requestTemperatures(); 
+  Celcius=sensors.getTempCByIndex(0);
+  Fahrenheit=sensors.toFahrenheit(Celcius);
+  Serial.print(" C  ");
+  Serial.print(Celcius);
+  Serial.print(" F  ");
+  Serial.println(Fahrenheit);
+  delay(1000);
 }
