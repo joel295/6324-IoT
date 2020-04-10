fd2af951b181abb8baa8ac2ab1bb0e0abf555512
Ultrivac <30451927+Ultrivac@users.noreply.github.com>
2020-04-10 15:34:41 +1000

    changed main to app.py, added some functionality

---
+++ azure_API/app.py
@@ -0,0 +1,109 @@
+
+from flask import Flask
+from flask_restplus import Resource, Api
+from bson import ObjectId
+from cosmos_db import get_collection, query_for_collections
+
+app = Flask(__name__)
+api = Api(app)
+
+# get the names of the available collections in the specified database
+@api.route('/api/v1/<string:database>/')
+class QueryCollections(Resource):
+
+    @api.response(200, 'OK')
+    @api.response(401, 'AUTHORIZATION FAILED')
+    @api.response(404, 'NOT FOUND')
+    def get(self, database):
+        query = query_for_collections(database)
+
+        if query == None:
+            return {
+                "message" : "authentification failed"
+            }, 401
+        if query == []:
+            return {
+                "message" : "database: {} not found or no collections".format(database)
+            }, 404
+
+        return_data = {
+            "collections" : query
+        }
+        return return_data, 200
+
+# get all documents from specified collection from specified database
+# there is one database per customer
+# a customers databse can have multiple devices each is its own collection
+@api.route('/api/v1/<string:database>/<string:collection>')
+class CollectAll(Resource):
+
+    @api.response(200, 'OK')
+    @api.response(401, 'AUTHORIZATION FAILED')
+    @api.response(404, 'NOT FOUND')
+    def get(self, database, collection):
+        my_collection = get_collection(database, collection)
+
+        # my_collection is None if authentification failed
+        if my_collection == None:
+            return {
+                "message" : "authentification failed"
+            }, 401
+        # == [] when my_collection with name collection could not be found in the specified database
+        # or database could not be found
+        elif [x for x in my_collection.find({})] == []:
+            return {
+                "message" : "database: {} or collection: {} not found".format(database, collection)
+            }, 404
+        # remove _id field from document, not json serialisable
+        return_list = []
+        for document in my_collection.find({}):
+            del document['_id']
+            return_list.append(document)
+
+        return return_list, 200
+
+# CollectDeviceSensor
+@api.route('/api/v1/<string:database>/<string:collection>/<string:field>')
+class CollectDeviceSensor(Resource):
+
+    @api.response(200, 'OK')
+    @api.response(401, 'AUTHORIZATION FAILED')
+    @api.response(404, 'NOT FOUND')
+    def get(self, database, collection, field):
+        my_collection = get_collection(database, collection)
+
+        # my_collection is None if authentification failed
+        if my_collection == None:
+            return {
+                "message" : "authentification failed"
+            }, 401
+        # == [] when my_collection with name collection could not be found in the specified database
+        # or database could not be found
+        elif [x for x in my_collection.find({})] == []:
+            return {
+                "message" : "database: {} or collection: {} not found".format(database, collection)
+            }, 404
+
+        # remove _id field from document, not json serialisable
+        # remove all sensors except sensor specified by field
+        # if field is mispelled, that document will have all sensors removed from data
+        # and the header data: epoch and device will exist but data = {}
+        return_list = []
+        for document in my_collection.find({}):
+            curr = {}
+            for key in document:
+                if key!='_id':
+                    curr[key] = document[key]
+            data = {}
+            for key in document['data']:
+                if key==field:
+                    data[key] = document['data'][key]
+            curr['data'] = data
+            return_list.append(curr)
+
+        return return_list, 200
+
+
+if __name__ == '__main__':
+    # run the application
+    app.run(debug=True)
--- azure_API/cosmos_db.py
+++ azure_API/cosmos_db.py
@@ -2,9 +2,9 @@
 from pymongo import MongoClient
 import os
 
-URI = "mongodb://wastewater:hF0wcz3mG9aamQ0vP6mlQOIJRVmXapAfLeQ8tS0CtMCNMJqIdVBfhdDcLPSzn0zAxWL2SIrVrTbIinWDXkmDSA==@wastewater.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@wastewater@"
-name = "wastewater"
-password = "hF0wcz3mG9aamQ0vP6mlQOIJRVmXapAfLeQ8tS0CtMCNMJqIdVBfhdDcLPSzn0zAxWL2SIrVrTbIinWDXkmDSA=="
+URI = "mongodb://wastewaterdb:1KE7cHuoAtZ7DkpCWe5Ozr8pSvCicuvF2FJAEtl9TPcNHAdGrnLU5ZY5zyo1Z9WuYjqJLq5aJJuRCWZSya1Vlw==@wastewaterdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@wastewaterdb@"
+name = "wastewaterdb"
+password = "1KE7cHuoAtZ7DkpCWe5Ozr8pSvCicuvF2FJAEtl9TPcNHAdGrnLU5ZY5zyo1Z9WuYjqJLq5aJJuRCWZSya1Vlw=="
 
 # provided database name and collection (both strings) return collection
 # database and collection need to be valid resource names from azure cosmos db
@@ -22,5 +22,21 @@ def get_collection(database, collection):
 
         my_collection = db[collection]
         return my_collection
+    except:
+        return None
+
+# given a valid database name, a list of collections in that database is returned
+def query_for_collections(database):
+    try:
+        # use for local testing
+        client = MongoClient(URI)
+        db = client[database] # select the database
+        db.authenticate(name=name, password=password)
+        # use for deployed use and fill credentials
+        # client = MongoClient(os.getenv("MONGOURL"))
+        # db = client.test
+        #db.authenticate(name=os.getenv("MONGO_USERNAME"),password=os.getenv("MONGO_PASSWORD"))
+
+        return db.list_collection_names()
     except:
         return None
